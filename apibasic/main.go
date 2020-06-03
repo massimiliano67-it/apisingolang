e1a349370de40b8a4fd5869b99fd6123404b1d39
massimiliano67-it <massimiliano.marocchi67@gmail.com>
2020-06-02 21:05:51 -0400

    add main and operation

---
+++ apibasic/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"apisingolang/apibasic/operation"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("helloworld: received a request")
+
+	//operation.Suma(2, 2)
+	fmt.Fprintf(w, "Hello Massimiliano Marocchi: la suma de %d+%d=%d!\n", 2, 2, operation.Suma(2, 2))
+
+}
+
+func main() {
+	log.Print("helloworld: starting server...")
+
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("helloworld: listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
