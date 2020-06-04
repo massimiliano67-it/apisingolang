65360488beed57d8935a5815f74b5c37a935edf8
massimiliano67-it <massimiliano.marocchi67@gmail.com>
2020-06-03 21:52:44 -0400

    last test

--- apibasic/main.go
+++ apibasic/main.go
@@ -12,7 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("helloworld Max: received a request")
 
 	//operation.Suma(2, 2)
-	fmt.Fprintf(w, "Hello Massimiliano Marocchi: la suma de %d+%d=%d!\n", 2, 2, operation.Suma(2, 2))
+	fmt.Fprintf(w, "Hello Massimiliano Marocchi: la suma de %d+%d=%d!\n", 3, 3, operation.Suma(3, 3))
 
 }
 
